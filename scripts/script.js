72d2bec721327151f343dd0bbab0d346ac4a52a6
Andrew <syong711@gmail.com>
2021-04-01 07:41:42 -0700

    Used for of loop to iterate through courseList array.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,44 @@
-let courseList=
+let course1= {
+    code: 'ACIT 1620',
+    name: 'Fundamental Web Technologies'
+};
+
+let course2= {
+    code: 'ACIT 1515',
+    name: 'Scripting for IT'
+};
+
+let course3= {
+    code: 'MATH 1310',
+    name: 'Technical Math for IT'
+};
+
+let courseList= [course1, course2, course3];
+
+
+let num;
+do{
+    num = prompt("Enter a four-digit code");
+} while(isNaN (num) === true || (num.length !== 4));
+
+
+
+
+for (let validcourse of courseList)
 {
-    code: ['ACIT 1620', 'ACIT 1515', 'MATH 1310'],
-    name: ['Fundamental Web Technologies', 'Scripting for IT', 'Technical Math for IT']
+    if (validcourse.code.indexOf("1620") === 0) {
+        console.log(`Yes I am taking the course: ${validcourse.code} - ${validcourse.name}`);
+    }
+    else if 
+
 }
 
 
-let user;
-do{
-    user = prompt("Enter a four-digit code");
-} while(isNaN (user) === true || (user.length !== 4));
 
+
+
+
+
+// courseList(code).push ("1630");
+// console.log("Database course added")
 
